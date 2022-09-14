f13a78c30c88a25da61acd34570e8193d08b1775
kanosawa <kanokanosawasawa@gmail.com>
2022-09-14 16:35:01 +0900

    update_mask_list関数を追加

--- peel/peel_test.py
+++ peel/peel_test.py
@@ -7,6 +7,29 @@ from layer_tree import LayerTree
 from image_creator import ImageCreator
 
 
+def update_mask_list(mask_list, descendant_nodes, model, rgb_img):
+
+    result = inference_segmentor(model, rgb_img)[0]
+
+    # 各画素の対応レイヤを記録した画像を生成
+    layer_idx_img = np.zeros((rgb_img.shape[0], rgb_img.shape[1]), dtype='uint16')
+    for i, node in enumerate(descendant_nodes):
+        if node.layer_data.visible:
+            _, _, _, alpha = node.layer_data.img.split()
+            alpha = np.array(alpha)
+            layer_idx_img[node.layer_data.bbox[1]: node.layer_data.bbox[3],
+            node.layer_data.bbox[0]: node.layer_data.bbox[2]][alpha >= 100] = i
+    layer_indices = np.unique(layer_idx_img)
+
+    # マスクリストに推論結果を記録
+    for layer_idx in layer_indices:
+        node = descendant_nodes[layer_idx]
+        bbox = node.layer_data.bbox
+        clipped_layer_idx_img = layer_idx_img[bbox[1]: bbox[3], bbox[0]: bbox[2]]
+        clipped_result = result[bbox[1]: bbox[3], bbox[0]: bbox[2]]
+        mask_list[layer_idx][clipped_layer_idx_img == layer_idx] = clipped_result[clipped_layer_idx_img == layer_idx]
+
+
 def main():
 
     config = '../configs/pspnet/pspnet_r50-d8_480x360_live2d.py'
@@ -28,25 +51,8 @@ def main():
     image_creator = ImageCreator()
     rgb_img = image_creator.create_image(layer_tree, label_flag=False)
     rgb_img = np.array(rgb_img)[:, :, :3][:, :, ::-1]
-    result = inference_segmentor(model, rgb_img)[0]
 
-    # 各画素の対応レイヤを記録した画像を生成
-    layer_idx_img = np.zeros((psd.height, psd.width), dtype='uint16')
-    for i, node in enumerate(descendant_nodes):
-        if node.layer_data.visible:
-            _, _, _, alpha = node.layer_data.img.split()
-            alpha = np.array(alpha)
-            layer_idx_img[node.layer_data.bbox[1]: node.layer_data.bbox[3],
-            node.layer_data.bbox[0]: node.layer_data.bbox[2]][alpha >= 100] = i
-    layer_indices = np.unique(layer_idx_img)
-
-    # マスクリストに推論結果を記録
-    for layer_idx in layer_indices:
-        node = descendant_nodes[layer_idx]
-        bbox = node.layer_data.bbox
-        clipped_layer_idx_img = layer_idx_img[bbox[1]: bbox[3], bbox[0]: bbox[2]]
-        clipped_result = result[bbox[1]: bbox[3], bbox[0]: bbox[2]]
-        mask_list[layer_idx][clipped_layer_idx_img == layer_idx] = clipped_result[clipped_layer_idx_img == layer_idx]
+    update_mask_list(mask_list, descendant_nodes, model, rgb_img)
 
     # 各レイヤの最終的な推論ラベルを決定
     infer_labels = []
@@ -56,6 +62,8 @@ def main():
             infer_labels.append(int(infer_label))
         else:
             infer_labels.append(0)
+
+    print('hoge')
 
 
 def extract_descendant_nodes(layer_tree):
